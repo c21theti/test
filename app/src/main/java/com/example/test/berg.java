a394b6b767312bfe892163b8330f952bfc0d9de5
c21theti <c21theti@student.his.se>
2022-04-08 16:43:49 +0200

    Testat klasser

---
+++ app/src/main/java/com/example/test/berg.java
@@ -0,0 +1,37 @@
+package com.example.test;
+
+public class berg {
+    private int height_m;
+    private String name;
+    private String location;
+
+    public berg(int height_m, String name, String location) {
+        this.height_m = height_m;
+        this.name = name;
+        this.location = location;
+    }  //alt+ enter -> enter
+
+    public int getHeight_m() {
+        return height_m;
+    }
+
+    public void setHeight_m(int height_m) {
+        this.height_m = height_m;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    } //alt + insert -> getter and setter
+}
